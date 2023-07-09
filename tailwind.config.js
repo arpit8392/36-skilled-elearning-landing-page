1bbce4ba8342c0f734627d9d932d801c9e1f4480
Arpit Namdev <arpit8392@gmail.com>
2023-07-09 13:41:31 +0530

    Add Colors in themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,14 @@ module.exports = {
 	],
 	theme: {
 		extend: {
+			colors: {
+				'primary-dark': '#13183F',
+				'secondary-1': '#83869A',
+				'secondary-2': '#666CA3',
+				'accent-1': '#F74780',
+				'accent-2': '#FFA7C3',
+				'primary-light': '#FFFFFF',
+			},
 			fontSize: {
 				'heading-xl': [
 					'3.5rem',
@@ -51,6 +59,12 @@ module.exports = {
 					},
 				],
 			},
+		},
+		backgroundImage: {
+			'primary-gradient':
+				'linear-gradient(180deg, #FF6F48 0.04%, #F02AA6 100%)',
+			'secondary-gradient': 'linear-gradient(180deg, #4851FF 0%, #F02AA6 100%)',
+			'light-gradient': 'linear-gradient(180deg, #FFF 0%, #F0F1FF 100%)',
 		},
 	},
 	plugins: [],
