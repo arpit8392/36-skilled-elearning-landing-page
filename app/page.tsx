9f7f87d66fa9e8f7e73165c1b9eddcf85cb85cd8
Arpit Namdev <arpit8392@gmail.com>
2023-07-09 21:34:24 +0530

    Fix Missing Footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
 import Courses from '@/components/Courses'
+import Footer from '@/components/Footer'
 import Header from '@/components/Header'
 import Hero from '@/components/Hero'
 
 export default function Home() {
 	return (
-		<main className='overflow-hidden'>
+		<main className='overflow-hidden scroll-smooth'>
 			<Header />
 			<Hero />
 			<Courses />
+			<Footer />
 		</main>
 	)
 }
