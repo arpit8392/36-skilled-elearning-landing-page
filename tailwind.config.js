597da63f3c605a73f7751fd6d2d8ebf4e2fd53ad
Arpit Namdev <arpit8392@gmail.com>
2023-07-09 13:31:18 +0530

    Typography Design System

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,57 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		'./pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		extend: {
+			fontSize: {
+				'heading-xl': [
+					'3.5rem',
+					{
+						lineHeight: '4.375',
+						fontWeight: '800',
+					},
+				],
+				'heading-lg': [
+					'2.5rem',
+					{
+						lineHeight: '3.188',
+						fontWeight: '800',
+					},
+				],
+				'heading-md': [
+					'2rem',
+					{
+						lineHeight: '2.5',
+						fontWeight: '800',
+					},
+				],
+				'heading-sm': [
+					'1.5rem',
+					{
+						lineHeight: '1.75',
+						fontWeight: '800',
+					},
+				],
+				'body-md': [
+					'1.125rem',
+					{
+						lineHeight: '1.75',
+						fontWeight: '500',
+					},
+				],
+				'body-sm': [
+					'1rem',
+					{
+						lineHeight: '1.75',
+						fontWeight: '700',
+					},
+				],
+			},
+		},
+	},
+	plugins: [],
 }
